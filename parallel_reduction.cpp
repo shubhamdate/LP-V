04306a3910760a541270f8a6bd91e9fa34e60f4f
Shubham Date <84425801+shubhamdate@users.noreply.github.com>
2023-04-13 10:08:29 +0530

    Add files via upload

---
+++ parallel_reduction.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<omp.h>
+#include <time.h>
+using namespace std;
+
+int sum(int a[],int n){
+	int sum = 0;
+	#pragma omp parallel for reduction(+:sum)
+	for (int i = 0; i<n ; i++){
+	sum += a[i];
+	}
+	return sum;
+}
+
+int min(int a[] , int n){
+	int v = a[0];
+	#pragma omp parallel for reduction(min:v)
+	for(int i = 0 ; i<n; i++){
+	if(a[i]< v)
+		v = a[i];
+	}
+	return v;
+}
+
+int max(int a[],int n){
+	int v = a[0];
+	#pragma omp parallel for reduction(max:v)
+	for(int i = 0 ; i<n ; i++)
+	{
+	if(a[i] > v)
+	{
+	v = a[i];
+	}
+	}
+	return v;
+}
+
+float avg(int a[], int n){
+	return sum(a,n)/n;
+}
+
+
+int main()	
+{
+
+	int a[100];
+	int cnt = 0;
+	for(int i = 0 ; i<100 ; i++)
+	{
+		a[i] = i + 5;
+		cnt = cnt + 1;
+	}
+	cout<<"Input data is : " ;
+	for(int i = 0 ; i<cnt ; i++)
+	{
+		cout<<" "<<a[i];
+	}
+	cout<<"\n";
+	cout<<"Sum : "<<sum(a,100);
+	cout<<"\n";
+	cout<<"Min : "<<min(a,100);
+	cout<<"\n";
+	cout<<"Max : "<<max(a,100);
+	cout<<"\n";
+	cout<<"Average : "<<avg(a,100);
+	cout<<"\n";
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
